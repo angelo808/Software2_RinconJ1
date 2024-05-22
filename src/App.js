bf2b6d87644eec9b9f4c845c166f01d0764376cb
Angelo Guillen <angeloguillen@Laptop-de-Angelo.local>
2024-05-22 00:08:40 -0500

    agregando dos ramas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Prueba from './prueba.js'; // Importamos el componente de la nueva pantalla
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Mi Aplicación React</h1>
+      <Prueba /> {/* Usamos el componente de la nueva pantalla */}
     </div>
   );
 }
-
 export default App;
