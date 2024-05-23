7924f65d81e18aa75b77dc2bd82803ec0fb63376
xabjhox <tesisjhonipanaque@gmail.com>
2024-05-23 14:30:01 -0500

    avance home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import Prueba from './prueba.js'; // Importamos el componente de la nueva pantalla
+import MyRoutes from './routes/MyRoutes.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Mi Aplicación React</h1>
-      <Prueba /> {/* Usamos el componente de la nueva pantalla */}
-    </div>
+    <MyRoutes />
   );
 }
 export default App;
