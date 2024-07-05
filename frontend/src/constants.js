8d9d5927d63655c8bc69f6950488a05bbc86b424
Alejandro Son Romero <alejandroson28@gmail.com>
2024-07-05 00:18:32 -0500

    cambio url

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,5 @@
 export const BLOQUEADO = "BLOQUEADO";
-export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5001/api/";
+export const API_URL = "http://localhost:5001/api/";
+
 
 
