6c32fbe878ad879fe98e20a3f1f95c0d7ce29421
Leonardo Trujillo <leobenavides187@gmail.com>
2024-07-09 20:39:50 -0500

    .

--- backend/routes/comments.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const commentController = require('../controllers/commentController');
-
-router.post('/comments', commentController.createComment);
-router.get('/posts/:postId', commentController.getCommentsByPostId);
-
-module.exports = router;
--- src/constants.js
+++
@@ -1,1 +0,0 @@
-export const BLOQUEADO = "BLOQUEADO";
